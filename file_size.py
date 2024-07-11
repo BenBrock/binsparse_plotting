d269e68f85b7751c62fc9d33d6ff99a6d39cea98
Benjamin Brock <benjamin.brock@intel.com>
2024-07-11 11:44:01 -0700

    Unify shared code in `plotting.py`

--- file_size.py
+++ file_size.py
@@ -1,68 +1,4 @@
-import matplotlib.pyplot as plt
-import csv
-
-def plot_sizes(datasets, labels, ordering, fname='out.pdf', title='', x_title='', y_title='', yticks=None):
-    fix,ax = plt.subplots()
-
-    dataset_values = [[dataset[matrix] for matrix in ordering] for dataset in datasets]
-
-    markers = ['s', '.', '^', 'o', '+']
-    for marker,dataset,label in zip(markers,dataset_values, labels):
-        domain = range(len(dataset))
-        y_points = dataset
-        ax.semilogy(domain, y_points, label=label)
-
-    ax.minorticks_off()
-
-    if yticks != None:
-        ax.set_yticks(yticks[0])
-        ax.set_yticklabels(yticks[1])
-
-    plt.title(title, fontsize=18)
-    plt.xlabel(x_title, fontsize=12)
-    plt.ylabel(y_title, fontsize=12)
-    plt.rcParams.update({'font.size': 12})
-    plt.legend(loc='best')
-
-    plt.tight_layout()
-    plt.savefig(fname)
-
-def read_dataset(fname):
-    data = {}
-    with open(fname, mode='r') as file:
-        csvFile = csv.DictReader(file)
-        for line in csvFile:
-            data[line['dataset']] = int(line['size_bytes'])
-    return data
-
-def read_nnz(fname):
-    data = {}
-    with open(fname, mode='r') as file:
-        csvFile = csv.DictReader(file)
-        for line in csvFile:
-            data[line['dataset']] = int(line['nnz'])
-    return data
-
-def pretty_print_size(size):
-    factor = 1
-    while size / 1024.0 >= 1:
-        factor *= 1024;
-        size /= 1024
-
-    if factor == 1:
-        label = 'B'
-    elif factor == 1024:
-        label = 'KiB'
-    elif factor == 1024*1024:
-        label = 'MiB'
-    elif factor == 1024*1024*1024:
-        label = 'GiB'
-    elif factor == 1024*1024*1024*1024:
-        label = 'TiB'
-    else:
-        assert(False)
-
-    return '%s %s' % (size, label)
+from plotting import *
 
 binsparse_coo_gzip9_aux = read_dataset('binsparse_coo_gzip9_aux.csv')
 
@@ -81,13 +17,11 @@ mtx_noz_noaux = read_dataset('mtx_noz_noaux.csv')
 
 matrix_nnz = read_nnz('matrix_nnzs.csv')
 
-cutoff = 0
-
-matrix_nnz = {dataset:nnz for dataset,nnz in matrix_nnz.items() if nnz > cutoff}
+cutoff = 1024*1024
 
 print(matrix_nnz)
 
-ordering = [x[0] for x in sorted(matrix_nnz.items(), key=lambda x: x[1], reverse=True)]
+ordering = [x[0] for x in sorted(mtx_noz_noaux.items(), key=lambda x: x[1], reverse=True) if matrix_nnz[x[0]] >= cutoff]
 
 datasets = [mtx_noz_noaux, binsparse_coo_noz_noaux, binsparse_coo_gzip1_noaux, binsparse_csr_noz_noaux, binsparse_csr_gzip1_noaux]
 labels = ['mtx_noz_noaux', 'binsparse_coo_noz_noaux', 'binsparse_coo_gzip1_noaux', 'binsparse_csr_noz_noaux', 'binsparse_csr_gzip1_noaux']
---
+++ plotting.py
@@ -0,0 +1,125 @@
+import matplotlib.pyplot as plt
+import csv
+import re
+
+from collections import defaultdict
+
+def plot_sizes(datasets, labels, ordering, fname='out.pdf', title='', x_title='', y_title='', yticks=None, style='scatter'):
+    fix,ax = plt.subplots()
+
+    dataset_values = [[dataset[matrix] for matrix in ordering] for dataset in datasets]
+
+    ax.set_yscale('log')
+
+    markers = ['s', '.', '^', 'o', '+']
+    for marker,dataset,label in zip(markers,dataset_values, labels):
+        domain = range(len(dataset))
+        y_points = dataset
+
+        if style == 'line':
+            ax.semilogy(domain, y_points, label=label)
+        elif style == 'scatter':
+            ax.scatter(domain, y_points, label=label, s=0.5)
+        else:
+            print('Style must be either line or scatter')
+            assert(False)
+
+    ax.minorticks_off()
+
+    if yticks != None:
+        ax.set_yticks(yticks[0])
+        ax.set_yticklabels(yticks[1])
+
+    plt.title(title, fontsize=18)
+    plt.xlabel(x_title, fontsize=12)
+    plt.ylabel(y_title, fontsize=12)
+    plt.rcParams.update({'font.size': 12})
+    plt.legend(loc='best')
+
+    plt.tight_layout()
+    plt.savefig(fname)
+
+def read_dataset(fname):
+    data = {}
+    with open(fname, mode='r') as file:
+        csvFile = csv.DictReader(file)
+        for line in csvFile:
+            data[line['dataset']] = int(line['size_bytes'])
+    return data
+
+def read_nnz(fname):
+    data = {}
+    with open(fname, mode='r') as file:
+        csvFile = csv.DictReader(file)
+        for line in csvFile:
+            data[line['dataset']] = int(line['nnz'])
+    return data
+
+def pretty_print_size(size):
+    factor = 1
+    while size / 1024.0 >= 1:
+        factor *= 1024;
+        size /= 1024
+
+    if factor == 1:
+        label = 'B'
+    elif factor == 1024:
+        label = 'KiB'
+    elif factor == 1024*1024:
+        label = 'MiB'
+    elif factor == 1024*1024*1024:
+        label = 'GiB'
+    elif factor == 1024*1024*1024*1024:
+        label = 'TiB'
+    else:
+        assert(False)
+
+    return '%s %s' % (size, label)
+
+
+def pretty_print_time(size):
+    factor = 1
+
+    if size >= 60:
+        label = 'm'
+        size /= 60
+    else:
+        if size >= 1:
+            label = 's'
+
+        if size < 1:
+            size *= 1000
+            label = 'ms'
+
+    if int(size) == size:
+        size = int(size)
+
+    return '%s %s' % (size, label)
+
+def read_benchmark_data(file_name):
+    matrix_data = defaultdict(lambda: {'runtime': [], 'bandwidth': []})
+
+    for line in open(file_name, 'r'):
+        if 'FORPARSER' in line:
+            m = re.match('FORPARSER: (.+)', line.strip())
+
+            if m:
+                data = m.group(1).split(',')
+
+                m2 = re.match('.+/(.+/.+?)\..+', data[0])
+                if m2:
+                  matrix = m2.group(1)
+
+                  runtime = float(data[1])
+                  bandwidth_gb = float(data[2])
+
+                  matrix_data[matrix]['runtime'].append(runtime)
+                  matrix_data[matrix]['bandwidth'].append(bandwidth_gb)
+                else:
+                    print('%s not matched...' % (data[0]))
+
+    return matrix_data
+
+def read_and_clean_benchmark_data(file_name):
+    matrix_data = read_benchmark_data(file_name)
+    return {x: sum(matrix_data[x]['runtime']) for x in matrix_data.keys()};
