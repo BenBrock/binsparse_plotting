c4f946a76e12e21c23827dd4e091db60076acb9c
Benjamin Brock <brock@cs.berkeley.edu>
2024-09-09 09:54:03 -0700

    Update to fix legend ordering to plot ordering

--- file_size_logx.py
+++ file_size_logx.py
@@ -27,8 +27,13 @@ cutoff = 1024*1024
 # Order by MTX file size
 ordering = [x[0] for x in sorted(mtx_noz_noaux.items(), key=lambda x: x[1], reverse=True) if matrix_nnz[x[0]] >= cutoff]
 
-datasets = [mtx_noz_noaux, binsparse_coo_noz_noaux, binsparse_coo_gzip1_noaux, binsparse_csr_noz_noaux, binsparse_csr_gzip1_noaux]
-labels = ['mtx_noz_noaux', 'binsparse_coo_noz_noaux', 'binsparse_coo_gzip1_noaux', 'binsparse_csr_noz_noaux', 'binsparse_csr_gzip1_noaux']
+# This was my old dataset ordering (keeping COO/COOGZ and CSR/CSRGZ consecutive).
+# The new legend ordering follows the top-bottom ordering on the plot.
+# datasets = [mtx_noz_noaux, binsparse_coo_noz_noaux, binsparse_coo_gzip1_noaux, binsparse_csr_noz_noaux, binsparse_csr_gzip1_noaux]
+# labels = ['mtx_noz_noaux', 'binsparse_coo_noz_noaux', 'binsparse_coo_gzip1_noaux', 'binsparse_csr_noz_noaux', 'binsparse_csr_gzip1_noaux']
+
+datasets = [mtx_noz_noaux, binsparse_coo_noz_noaux, binsparse_csr_noz_noaux, binsparse_coo_gzip1_noaux, binsparse_csr_gzip1_noaux]
+labels = ['mtx_noz_noaux', 'binsparse_coo_noz_noaux', 'binsparse_csr_noz_noaux', 'binsparse_coo_gzip1_noaux', 'binsparse_csr_gzip1_noaux']
 
 # datasets = [mtx_noz_noaux, binsparse_coo_noz_noaux, binsparse_coo_gzip1_noaux, binsparse_coo_gzip9_noaux]
 # labels = ['mtx_noz_noaux', 'binsparse_coo_noz_noaux', 'binsparse_coo_gzip1_noaux', 'binsparse_coo_gzip9_noaux']
@@ -39,9 +44,9 @@ ytick_labels = [pretty_print_size(x) for x in ytick_data]
 xtick_data = [x * 1024 * 1024 for x in [16, 64, 256, 1024, 4096, 16384, 65536]]
 xtick_labels = [pretty_print_size(x) for x in xtick_data]
 
-colors = ['C0', 'C1', 'C3', 'C2', 'C4']
+colors = ['C0', 'C1', 'C2', 'C3', 'C4']
 
-plot_sizes_logx(mtx_noz_noaux, datasets, labels, ordering, title='File Size - SuiteSparse Matrix Collection', y_title='File Size (Bytes)', x_title='Matrix Market File Size', yticks = (ytick_data,ytick_labels), xticks = (xtick_data, xtick_labels), colors=colors, fname='out.png')
+plot_sizes_logx(mtx_noz_noaux, datasets, labels, ordering, title='File Size - SuiteSparse Matrix Collection', y_title='File Size (Bytes)', x_title='Matrix Market File Size', yticks = (ytick_data,ytick_labels), xticks = (xtick_data, xtick_labels), colors=colors, fname='out.pdf')
 
 print(ytick_data)
 print(ytick_labels)
